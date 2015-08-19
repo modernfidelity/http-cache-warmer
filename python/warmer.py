08a36d3f4d025f240806a88c2964b32e22a7bd02
Mark Rushton <mark@modernfidelity.co.uk>
2015-08-19 11:32:55 +0100

    add cli helpers for args

--- python/warmer.py
+++ python/warmer.py
@@ -7,10 +7,11 @@ from urllib2 import urlopen
 import pycurl
 from multiprocessing import Pool
 from StringIO import StringIO
-
+import argparse
 
 
 #
+# Main GET request via cURL
 #
 def curlUrl(requestUrl):
     buffer = StringIO()
@@ -18,69 +19,95 @@ def curlUrl(requestUrl):
     c = pycurl.Curl()
     c.setopt(c.URL, requestUrl)
     c.setopt(c.WRITEDATA, buffer)
+
     c.perform()
+
+    status = c.getinfo(pycurl.HTTP_CODE)
+    effectiveURL = c.getinfo(pycurl.EFFECTIVE_URL)
+
     c.close()
 
-    return buffer.getvalue()
+    print status, effectiveURL
+
+    # return c.getinfo(pycurl.HTTP_CODE), c.getinfo(pycurl.EFFECTIVE_URL)
+    # return buffer.getvalue()
+
+
+#
+# Load a batch of URLs from a file
+#
+
+def loadBatchFile(file):
+    return open(file)
 
 
 #
 #
 def getSiteXml(requestUrl):
-    requestUrl = requestUrl + "/sitemap.xml"
-
-    r = requests.get(requestUrl)
-
-    data = r.text
-
-    soup = BeautifulSoup(data, 'html.parser')
-
-    collection = soup.findAll("loc")
-
-    return collection
+    return
 
 
 #
 #
 def getUrl(url):
-    return curlUrl(url)
+    return
 
 
 #
 #
 def parseXml(url):
-    url = url + "/sitemap.xml"
-
-    r = requests.get(url)
-
-    data = r.text
-
-    tree = ET.parse(data)
-
-    root = tree.getroot()
-
-    collection = []
-
-    for loc in root.findAll("loc"):
-        collection.append(loc.text)
-
-        # return collection
+    return
 
 
+#
+# Main functionality
+#
 if __name__ == '__main__':
 
-    website = sys.argv[1]
+    # Add CLI helpers
+    parser = argparse.ArgumentParser(
+        description='HTTP CACHE WARMER :: Lets warm things up a bit...'
+    )
 
-    URL = website + "/sitemap.xml"
+    parser.add_argument('--url', help='The URL to test')
 
-    print URL
+    parser.add_argument('--file', help='The batch file of URLs to process')
 
-    tree = ET.parse(urlopen(URL))
+    args = parser.parse_args()
 
-    root = tree.getroot()
+    # Check for Batch file
+    if args.file:
 
-    ns = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
 
-    for url in root.findall('sitemap:url', ns):
-        loc = url.find('sitemap:loc', ns)
-        print loc.text
+        print "@todo - file = " + args.file
+
+
+
+    # Default to provided URL (full)
+    else:
+
+        print "Requesting URL : " + args.url
+
+        URL = args.url + "/sitemap.xml"
+
+        tree = ET.parse(urlopen(URL))
+
+        root = tree.getroot()
+
+        ns = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+
+        #
+        for url in root.findall('sitemap:url', ns):
+            loc = url.find('sitemap:loc', ns)
+
+            curlUrl(loc.text)
+
+
+            # -------------------------
+
+            # website = sys.argv[1]
+            #
+            # URL = website + "/sitemap.xml"
+            #
+            # print URL
+            #
